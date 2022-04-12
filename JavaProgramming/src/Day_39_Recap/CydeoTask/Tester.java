3e53bc833bdd21918586bf333ec112a6464a4cab
Alex <fatihbalci.phd@gmail.com>
2022-04-12 16:16:23 +0300

    Cars

---
+++ JavaProgramming/src/Day_39_Recap/CydeoTask/Tester.java
@@ -0,0 +1,17 @@
+package Day_39_Recap.CydeoTask;
+
+public class Tester extends Employee {
+
+    public Tester(String name, int age, char gender, int id, String jobTitle, double salary) {
+        super(name, age, gender, id, jobTitle, salary);
+    }
+
+    public void createTicket(){
+        System.out.println(getJobTitle()+" "+getName()+" is creating ticket.");
+    }
+
+
+
+
+
+}
